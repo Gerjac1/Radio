42114c58a4d803c434aabeb047f005e822ae13b6
Gerjac1 <160284510+Gerjac1@users.noreply.github.com>
2024-02-19 13:51:27 +0100

    Update

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function ChangeImage(){
     }
     else playbutton.classList.add("Stop");
 }
+
+function SlideBar(){
+    $('.slider').on( 'input', function( ) {
+        if(this.value<50){
+            $( this ).css( 'background', 'linear-gradient(to right, rgba(6,223,23,1) '+this.value+'%, grey 0%');
+        } else{
+            $( this ).css( 'background', 'linear-gradient(to right, rgba(6,223,23,1)' + this.value + '%, grey 50%');
+        }
+    });
+}
