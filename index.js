b5f9a6cc356fa43c2948b218b3df1c1ee0dbd8e8
Gerjac1 <160284510+Gerjac1@users.noreply.github.com>
2024-02-21 09:44:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function ChangeImage(){
     else playbutton.classList.add("Stop");
 }
 
+function volume(){
+    var RangeValue = document.getElementById("myRange").value;
+    var wynik = document.getElementById("volume");
+    wynik.innerHTML="Głośność: "+RangeValue+"%";
+}
+
 function SlideBar(){
     $('.slider').on( 'input', function( ) {
         if(this.value<50){
@@ -15,4 +21,11 @@ function SlideBar(){
             $( this ).css( 'background', 'linear-gradient(to right, rgba(6,223,23,1)' + this.value + '%, grey 50%');
         }
     });
+}
+
+function Mute(){
+    var Range = document.getElementById("myRange");
+    Range.value=0;
+    volume();
+    document.getElementById("myRange").style.background='linear-gradient(to right, rgba(6,223,23,1) 0%, grey 0%)';
 }
