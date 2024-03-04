1308a81111a9d40ebc0736220b366af7ff4a030d
Gerjac1 <160284510+Gerjac1@users.noreply.github.com>
2024-03-04 12:57:27 +0100

    Fix for RMF Classic

---
+++ current_song.php
@@ -0,0 +1,31 @@
+<?php
+//Sprawdź która stacja z listy jest odtwarzana
+$output=null;
+$retvalue=null;
+$command="mpc status |grep playing |cut -d# -f2 |cut -d/ -f1";
+exec($command,$output,$retvalue);
+$stacja=$output[0];
+//echo $stacja;
+switch ($stacja) {
+    case "2":
+        $output=null;
+        $retvalue=null;
+        $command="mpc status |head -n 1 | cut -d: -f2- |cut -d '|' -f1,1";
+        exec($command,$output,$retvalue);
+        $opis=$output[0];
+        break;
+    default:
+        $output=null;
+        $retvalue=null;
+        $command="mpc status |head -n 1 |cut -d ':' -f2- |cut -d' ' -f2-";
+        exec($command,$output,$retvalue);
+        $opis=$output[0];
+        break;
+}
+$opis="Utwór: ".substr($opis,0,60);
+if(strpos($opis, "n/a")) {
+    $opis = "Aktualnie nie gra żadna muzyka";
+    echo $opis;
+}
+else echo $opis;
+?>
